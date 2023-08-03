672c53acbe7a8d5f3c8910f9f0ed6259b18c720f
KafCoppelia <k740677208@gmail.com>
2023-08-03 19:48:05 +0800

    🎨 enhancement: improved `Coord`

--- paitest/frames/coord.py
+++ paitest/frames/coord.py
@@ -1,8 +1,14 @@
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Union, overload
 
 
 class Coord:
-    """Coordinates of cores"""
+    """Coordinates of the cores. Set coordinates (x, y) for every cores.
+
+    Left to right, +X, up to down, +Y.
+    """
+
+    _COORD_MAX_LIMIT = (1 << 5) - 1
+    _COORD_LOW_LIMIT = 0
 
     def __init__(
         self, _x: Union[Tuple[int, int], int], _y: Optional[int] = None
@@ -19,67 +25,263 @@ class Coord:
         else:
             raise ValueError("Wrong Argument")
 
-        if not (0 <= x < 32 and 0 <= y < 32):
-            raise ValueError(f"0 <= x < 32, 0 <= y < 32: ({x}, {y})")
+        if not (self._COORD_LOW_LIMIT <= x <= self._COORD_MAX_LIMIT and self._COORD_LOW_LIMIT <= y <= self._COORD_MAX_LIMIT):
+            raise ValueError(f"{self._COORD_LOW_LIMIT} <= x <= {self._COORD_MAX_LIMIT}, {self._COORD_LOW_LIMIT} <= y <= {self._COORD_MAX_LIMIT}: ({x}, {y})")
 
         self.x: int = x
         self.y: int = y
 
-    def __add__(self, other) -> "Coord":
-        return Coord(self.x + other.x, self.y + other.y)
+    @classmethod
+    def from_tuple(cls, pos) -> "Coord":
+        return cls(*pos)
 
-    def __sub__(self, other) -> "CoordOffset":
-        return CoordOffset(self.x - other.x, self.y - other.y)
+    @classmethod
+    def default(cls) -> "Coord":
+        return cls(0, 0)
 
-    def __eq__(self, other) -> bool:
-        if isinstance(other, Tuple):
-            return (self.x, self.y) == other
+    def __add__(self, __other: "CoordOffset") -> "Coord":
+        """
+        Examples:
 
-        return self.x == other.x and self.y == other.y
+        Coord = Coord + CoordOffset
+        >>> c1 = Coord(1, 1)
+        >>> c2 = c1 + CoordOffset(1, 1)
+        c1: Coord(2, 2)
 
-    def __ne__(self, other) -> bool:
-        return self.x != other.x or self.y != other.y
+        NOTE: Coord + Coord is meaningless.
+        """
+        if not isinstance(__other, CoordOffset):
+            raise TypeError(f"Unsupported type: {type(__other)}")
 
-    def __lt__(self, other) -> bool:
-        """Whether on the left or below"""
-        return self.x < other.x or self.y < other.y
+        return Coord(self.x + __other.delta_x, self.y + __other.delta_y)
 
-    def __gt__(self, other) -> bool:
-        """Whether on the right and above"""
+    @overload
+    def __sub__(self, __other: "Coord") -> "CoordOffset":
+        ...
+
+    @overload
+    def __sub__(self, __other: "CoordOffset") -> "Coord":
+        ...
+
+    def __sub__(
+        self, __other: Union["Coord", "CoordOffset"]
+    ) -> Union["Coord", "CoordOffset"]:
+        """
+        Example:
+        >>> c1 = Coord(1, 1)
+        >>> c2 = Coord(2, 2) - c1
+        c2: CoordOffset(1, 1)
+        """
+        if isinstance(__other, Coord):
+            return CoordOffset(self.x - __other.x, self.y - __other.y)
+
+        if isinstance(__other, CoordOffset):
+            return Coord(self.x - __other.delta_x, self.y - __other.delta_y)
+
+        raise TypeError(f"Unsupported type: {type(__other)}")
+
+    """Operations below are used only when comparing with a Cooord."""
+
+    def __eq__(self, __other: "Coord") -> bool:
+        """
+        Example:
+        >>> Coord(4, 5) == Coord(4, 6)
+        False
+        """
+        if not isinstance(__other, Coord):
+            raise TypeError(f"Unsupported type: {type(__other)}")
+
+        return self.x == __other.x and self.y == __other.y
+
+    def __ne__(self, __other: "Coord") -> bool:
+        """
+        Examples:
+        >>> Coord(4, 5) != Coord(4, 6)
+        True
+
+        >>> Coord(4, 5) != Coord(5, 5)
+        True
+        """
+        if not isinstance(__other, Coord):
+            raise TypeError(f"Unsupported type: {type(__other)}")
+
+        return self.x != __other.x or self.y != __other.y
+
+    def __lt__(self, __other: "Coord") -> bool:
+        """Whether the coord is on the left OR below of __other.
+
+        Examples:
+        >>> Coord(4, 5) < Coord(4, 6)
+        True
+
+        >>> Coord(4, 5) < Coord(5, 5)
+        True
+
+        >>> Coord(4, 5) < Coord(5, 3)
+        True
+        """
+        if not isinstance(__other, Coord):
+            raise TypeError(f"Unsupported type: {type(__other)}")
+
+        return self.x < __other.x or self.y < __other.y
+
+    def __gt__(self, __other: "Coord") -> bool:
+        """Whether the coord is on the right AND above of __other.
+
+        Examples:
+        >>> Coord(5, 5) > Coord(4, 5)
+        True
+
+        >>> Coord(4, 6) > Coord(4, 5)
+        True
+
+        >>> Coord(5, 4) > Coord(4, 5)
+        False
+        """
+        if not isinstance(__other, Coord):
+            raise TypeError(f"Unsupported type: {type(__other)}")
+
+        # Except the `__eq__`
         return (
-            (self.x > other.x and self.y > other.y)
-            or (self.x == other.x and self.y > other.y)
-            or (self.x > other.x and self.y == other.y)
+            (self.x > __other.x and self.y > __other.y)
+            or (self.x == __other.x and self.y > __other.y)
+            or (self.x > __other.x and self.y == __other.y)
         )
 
-    def __le__(self, other) -> bool:
-        return self.__lt__(other) or self.__eq__(other)
+    def __le__(self, __other: "Coord") -> bool:
+        return self.__lt__(__other) or self.__eq__(__other)
 
-    def __ge__(self, other) -> bool:
-        return self.__gt__(other) or self.__eq__(other)
+    def __ge__(self, __other: "Coord") -> bool:
+        return self.__gt__(__other) or self.__eq__(__other)
 
     def __str__(self) -> str:
         return f"({self.x}, {self.y})"
 
+    def __repr__(self) -> str:
+        return f"Coord({self.x}, {self.y})"
 
-class CoordOffset(Coord):
+    def to_tuple(self) -> Tuple[int, int]:
+        """Convert to tuple"""
+        return (self.x, self.y)
+
+    def _to_address(self) -> int:
+        """Convert to address, 10 bits"""
+        return (self.x << 5) | self.y
+
+    @property
+    def address(self) -> int:
+        return self._to_address()
+
+
+class CoordOffset:
     """Offset of coordinates"""
 
-    def __init__(self, _x: int, _y: int) -> None:
-        if not (-32 < _x < 32 and -32 < _y < 32):
-            raise ValueError(f"-32 < x < 32, -32 < y < 32: ({_x}, {_y})")
+    _COORDOFFSET_MAX_LIMIT = (1 << 5) - 1
+    _COORDOFFSET_LOW_LIMIT = -(1 << 5)
 
-        self.x: int = _x
-        self.y: int = _y
+    def __init__(self, _delta_x: int, _delta_y: int) -> None:
+        if not (self._COORDOFFSET_LOW_LIMIT < _delta_x <= self._COORDOFFSET_MAX_LIMIT and self._COORDOFFSET_LOW_LIMIT < _delta_y <= self._COORDOFFSET_MAX_LIMIT):
+            raise ValueError(f"{self._COORDOFFSET_LOW_LIMIT} < delta_x <= {self._COORDOFFSET_MAX_LIMIT}, {self._COORDOFFSET_LOW_LIMIT} < delta_y <= {self._COORDOFFSET_MAX_LIMIT}: ({_delta_x}, {_delta_y})")
 
-    def __add__(self, other) -> Union["CoordOffset", Coord]:
-        if isinstance(other, CoordOffset):
-            return CoordOffset(self.x + other.x, self.y + other.y)
-        else:
-            return Coord(self.x + other.x, self.y + other.y)
+        self.delta_x: int = _delta_x
+        self.delta_y: int = _delta_y
 
-    def __sub__(self, other) -> "CoordOffset":
-        if isinstance(other, Coord):
-            raise TypeError("A CoordOffset cannot substract a Coord")
+    @overload
+    def __add__(self, __other: Coord) -> Coord:
+        ...
 
-        return CoordOffset(self.x - other.x, self.y - other.y)
+    @overload
+    def __add__(self, __other: "CoordOffset") -> "CoordOffset":
+        ...
+
+    def __add__(
+        self, __other: Union["CoordOffset", Coord]
+    ) -> Union["CoordOffset", Coord]:
+        """
+        Examples:
+        >>> delta_c1 = CoordOffset(1, 1)
+        >>> delta_c2 = delta_c1 + CoordOffset(1, 1)
+        delta_c2: CoordOffset(2, 2)
+
+        Coord = CoordOffset + Coord
+        >>> delta_c = CoordOffset(1, 1)
+        >>> c1 = Coord(2, 3)
+        >>> c2 = delta_c + c1
+        c2: Coord(3, 4)
+        """
+        if isinstance(__other, CoordOffset):
+            return CoordOffset(
+                self.delta_x + __other.delta_x, self.delta_y + __other.delta_y
+            )
+
+        if isinstance(__other, Coord):
+            return Coord(self.delta_x + __other.x, self.delta_y + __other.y)
+
+        raise TypeError(f"Unsupported type: {type(__other)}")
+
+    def __iadd__(self, __other: "CoordOffset") -> "CoordOffset":
+        """
+        Example:
+        >>> delta_c = CoordOffset(1, 1)
+        >>> delta_c += CoordOffset(1, 1)
+        delta_c: CoordOffset(2, 2)
+        """
+        if not isinstance(__other, CoordOffset):
+            raise TypeError(f"Unsupported type: {type(__other)}")
+
+        self.delta_x += __other.delta_x
+        self.delta_y += __other.delta_y
+
+        return self
+
+    def __sub__(self, __other: "CoordOffset") -> "CoordOffset":
+        """
+        Example:
+        >>> delta_c1 = CoordOffset(1, 1)
+        >>> delta_c2 = CoordOffset(2, 2)
+        >>> delta_c = delta_c1 - delta_c2
+        delta_c: CoordOffset(-1, -1)
+        """
+        if not isinstance(__other, CoordOffset):
+            raise TypeError(f"Unsupported type: {type(__other)}")
+
+        return CoordOffset(
+            self.delta_x - __other.delta_x, self.delta_y - __other.delta_y
+        )
+
+    def __isub__(self, __other: "CoordOffset") -> "CoordOffset":
+        """
+        Example:
+        >>> delta_c = CoordOffset(1, 1)
+        >>> delta_c -= CoordOffset(1, 1)
+        delta_c: CoordOffset(0, 0)
+        """
+        if not isinstance(__other, CoordOffset):
+            raise TypeError(f"Unsupported type: {type(__other)}")
+
+        self.delta_x -= __other.delta_x
+        self.delta_y -= __other.delta_y
+
+        return self
+
+    def __eq__(self, __other: "CoordOffset") -> bool:
+        """
+        Example:
+        >>> CoordOffset(4, 5) == CoordOffset(4, 6)
+        False
+        """
+        if not isinstance(__other, CoordOffset):
+            raise TypeError(f"Unsupported type: {type(__other)}")
+
+        return self.delta_x == __other.delta_x and self.delta_y == __other.delta_y
+
+    def __ne__(self, __other: "CoordOffset") -> bool:
+        """
+        Example:
+        >>> CoordOffset(4, 5) != CoordOffset(4, 6)
+        True
+        """
+        if not isinstance(__other, CoordOffset):
+            raise TypeError(f"Unsupported type: {type(__other)}")
+
+        return self.delta_x != __other.delta_x or self.delta_y != __other.delta_y
