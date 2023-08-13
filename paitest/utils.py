dd284c976eae155ab8f670dab9fd068e121d6dde
KafCoppelia <k740677208@gmail.com>
2023-08-13 20:06:06 +0800

    🛠️ add `bin_combine_x`

--- paitest/utils.py
+++ paitest/utils.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple, Union
 from paitest._types import CoordLike
 from paitest.coord import Coord, CoreType
 
@@ -45,28 +45,57 @@ def bin_split(x: int, pos: int, high_mask: Optional[int] = None) -> Tuple[int, i
     return high, low
 
 
-def bin_combine(high: int, low: int, pos: Optional[int] = None) -> int:
+def bin_combine(high: int, low: int, pos: int) -> int:
     """Combine two integers, return the result.
 
     Argument:
-        - high: the integer on the high bit
-        - low: the integer on the low bit
-        - pos: the combination bit if provided. Otherwise the position is `low.bit_length()`
+        - high: the integer on the high bit.
+        - low: the integer on the low bit.
+        - pos: the combination bit if provided. Must be equal or greater than `low.bit_length()`.
 
     Example: combine 0b11000, 0b101
     >>> bin_combine(0b11000, 0b101, 5)
-    773(0b1100000101)
-
-    >>> bin_combine(0b11000, 0b101)
-    197(0b11000101)
+    773(0b11000_00101)
     """
+    if pos < 0:
+        raise ValueError("position must be greater than 0")
 
-    if isinstance(pos, int):
-        if pos < low.bit_length():
-            raise ValueError(f"Postion of combination must greater than the bit length of low({low.bit_length()})")
-        _pos = pos
-    
+    if low > 0 and pos < low.bit_length():
+        raise ValueError(
+            f"Postion of combination must be greater than the bit length of low({low.bit_length()})"
+        )
+
+    return (high << pos) + low
+
+
+def bin_combine_x(*components: int, pos: Union[int, List[int], Tuple[int, ...]]) -> int:
+    """Combine more than two integers, return the result.
+
+    Argument:
+        - components: the list of integers to be combined.
+        - pos: the combination bit(s) if provided. Every bit must be equal or greater than `low.bit_length()`.
+
+    Example: combine 0b11000, 0b101, 0b01011
+    >>> bin_combine_x(0b11000, 0b101, 0b1011, pos=[10, 5])
+    24747(0b11000_00101_01011)
+    """
+    if isinstance(pos, (list, tuple)):
+        if len(components) != len(pos) + 1:
+            raise ValueError(
+                f"Length of components and positions illegal: {len(components)}, {len(pos)}"
+            )
     else:
-        _pos = low.bit_length()
+        if len(components) != 2:
+            raise ValueError(
+                f"Length of components must be 2: {len(components)} when position is an integer."
+            )
 
-    return (high << _pos) + low
+        return bin_combine(*components, pos=pos)
+
+    result = components[-1]
+
+    # Traverse every position from the end to the start
+    for i in range(len(pos)-1, -1, -1):
+        result = bin_combine(components[i], result, pos[i])
+
+    return result
